6f50cc58aa3f8cb5b2257e298036165b34924f48
geraldesteban <wakin.esteban@gmail.com>
2026-03-16 11:18:50 +0800

    chore: add footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="text-center text-gray-500 py-5 border-t-[0.1px]">
+    <footer className="text-center text-gray-500 border-t-[0.1px] py-5">
       © 2026 Gerald Esteban
     </footer>
   );
