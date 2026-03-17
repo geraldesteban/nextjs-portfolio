1d8f59679d30fa52fd6cc4a1e5bb50ebc1ab21d3
geraldesteban <wakin.esteban@gmail.com>
2026-03-18 02:49:25 +0800

    feat: add details and status data

--- app/data/featuredProjects.ts
+++ app/data/featuredProjects.ts
@@ -13,47 +13,59 @@ export const featuredProjects: FeaturedProjectsTypes[] = [
     title: "React Personal Finance App",
     description:
       "A Full Stack Personal Finance app, a challenge from Frontend Mentor at Guru difficulty.",
+    details: "reactpersonalfinanceapp",
     repo: "https://github.com/geraldesteban/react-personal-finance-app",
     link: "https://gce-react-personal-finance.vercel.app/login",
+    status: "Finished",
   },
   {
     image: NextJSAudiophileEcommerce,
     title: "Next.js Audiophile App",
     description:
       "A Full Stack Audiophile E-Commerce app, a challenge from Frontend Mentor at Guru difficulty.",
+    details: "nextjsaudiophileecommerce",
     repo: "https://github.com/geraldesteban/nextjs-audiophile-app",
     link: "https://github.com/geraldesteban/nextjs-audiophile-app",
+    status: "Ongoing",
   },
   {
     image: ReactTWO,
     title: "React The Wild Oasis App",
     description:
       "A Full Stack Wild Oasis app (Admin POV) built with React from a Udemy tutorial.",
+    details: "reactthewildoasisapp",
     repo: "https://github.com/geraldesteban/nextjs-the-wild-oasis",
     link: "https://gce-react-the-wild-oasis.vercel.app/",
+    status: "Finished",
   },
   {
     image: NextJSTWO,
     title: "Next.js The Wild Oasis App",
     description:
       "A Full Stack Wild Oasis app (User POV) built with Next.js from a Udemy tutorial.",
+    details: "nextjsthewildoasisapp",
     repo: "https://github.com/geraldesteban/nextjs-the-wild-oasis",
     link: "https://gce-nextjs-the-wild-oasis.vercel.app/",
+    status: "Finished",
   },
   {
     image: NextJSWeddingInvitation,
     title: "Next.js Wedding Invitation App",
     description:
-      "A Wedding Invitation app built with Next.js, Tailwind CSS, and Supabase.",
+      " A Wedding Invitation App where guests can view event details and submit their RSVP.",
+    details: "nextjsweddinginvitationapp",
     repo: "https://github.com/geraldesteban/wedding-invitation-one",
     link: "https://aris-jane-wedding.vercel.app/",
+    status: "Finished",
   },
   {
     image: RestCountriesAPI,
     title: "REST Countries API App",
     description:
-      "A REST Countries API app built with JavaScript, Tailwind CSS, and REST Countries API.",
+      "A REST Countries API where users can view short details of the countries.",
+    details: "restcountriesapiapp",
     repo: "https://github.com/geraldesteban/project-rest-countries-api",
     link: "https://rest-countries-api-gce.vercel.app/",
+    status: "Finished",
   },
 ];
