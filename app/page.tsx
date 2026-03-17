88e7ead442b9926dcf92196a216a9da4bb4e4eaa
geraldesteban <wakin.esteban@gmail.com>
2026-03-18 02:50:46 +0800

    chore: move header and footer from layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,14 @@ import TechStack from "@/app/_components/Sections/TechStack";
 import FeaturedProjects from "@/app/_components/Sections/Projects/FeaturedProjects";
 import OtherProjects from "@/app/_components/Sections/Projects/OtherProjects";
 import About from "@/app/_components/Sections/About";
+import Header from "./_components/Header";
+import Footer from "./footer";
 
 function Page() {
   return (
     <main className="text-[#d9d9d9]">
+      {/* Navigation Bar */}
+      <Header />
       {/* About */}
       <About />
       {/* Tech Stack */}
@@ -14,6 +18,8 @@ function Page() {
       <FeaturedProjects />
       {/* Other Projects */}
       <OtherProjects />
+      {/* Footer */}
+      <Footer />
     </main>
   );
 }
