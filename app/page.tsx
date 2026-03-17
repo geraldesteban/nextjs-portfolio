968a2fc1fd4c5a5ee6f8f7cc5e6febfb1c6a1775
geraldesteban <wakin.esteban@gmail.com>
2026-03-17 20:34:47 +0800

    feat: add the sections (about, tech stack, featured projects, and other projects) created from components/sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,19 @@
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import EmailIcon from "@mui/icons-material/Email";
-import ArticleIcon from "@mui/icons-material/Article";
+import TechStack from "@/app/_components/Sections/TechStack";
+import FeaturedProjects from "@/app/_components/Sections/Projects/FeaturedProjects";
+import OtherProjects from "@/app/_components/Sections/Projects/OtherProjects";
+import About from "@/app/_components/Sections/About";
 
-/* Home Page */
 function Page() {
   return (
     <main className="text-[#d9d9d9]">
-      <div className="flex flex-col gap-3">
-        <div className="flex flex-col gap-2.5">
-          <h2 className="font-bold text-5xl">Hi, I&apos;m Gerald Esteban</h2>
-          <p className="text-base">
-            A Web Developer who is experienced in building web applications with
-            JavaScript, TypeScript, React, Next.js, and Supabase.
-          </p>
-        </div>
-        <div className="text-base flex gap-5">
-          <a
-            href="https://github.com/geraldesteban"
-            className="flex gap-1 items-center p-1 border-[1px] rounded-sm"
-            target="_blank"
-          >
-            <GitHubIcon />
-            GITHUB
-          </a>
-          <a
-            href="https://www.linkedin.com/in/geraldesteban"
-            className="flex gap-1 items-center p-1 border-[1px] rounded-sm"
-            target="_blank"
-          >
-            <LinkedInIcon />
-            LINKEDIN
-          </a>
-          <a
-            href="#"
-            className="flex gap-1 items-center p-1 border-[1px] rounded-sm"
-            target="_blank"
-          >
-            <EmailIcon />
-            EMAIL
-          </a>
-          <a
-            href="#"
-            className="flex gap-1 items-center p-1 border-[1px] rounded-sm"
-            target="_blank"
-          >
-            <ArticleIcon />
-            RESUME
-          </a>
-        </div>
-      </div>
+      {/* About */}
+      <About />
+      {/* Tech Stack */}
+      <TechStack />
+      {/* Featured Projects */}
+      <FeaturedProjects />
+      {/* Other Projects */}
+      <OtherProjects />
     </main>
   );
 }
