474898e71fdf907d3455305c0560d04661e5635c
geraldesteban <wakin.esteban@gmail.com>
2026-03-17 20:31:20 +0800

    chore: add data for featured projects and other projects

--- app/data/featuredProjects.ts
+++ app/data/featuredProjects.ts
@@ -1,11 +1,13 @@
-import { Project } from "@/app/types/featuredProjects";
+import { FeaturedProjectsTypes } from "@/app/types/featuredProjects";
 
 import ReactPersonalFinance from "@/app/_assets/FeaturedProjects/ReactPersonalFinance.png";
+import NextJSAudiophileEcommerce from "@/app/_assets/FeaturedProjects/NextJSAudiophileEcommerce.png";
+import ReactTWO from "@/app/_assets/FeaturedProjects/ReactTWO.png";
 import NextJSTWO from "@/app/_assets/FeaturedProjects/NextJSTWO.png";
-import ReactWeddingInvitation from "@/app/_assets/FeaturedProjects/WeddingInvitation.png";
+import NextJSWeddingInvitation from "@/app/_assets/FeaturedProjects/NextJSWeddingInvitation.png";
 import RestCountriesAPI from "@/app/_assets/FeaturedProjects/RestCountriesAPI.png";
 
-export const featuredProjects: Project[] = [
+export const featuredProjects: FeaturedProjectsTypes[] = [
   {
     image: ReactPersonalFinance,
     title: "React Personal Finance App",
@@ -15,42 +17,42 @@ export const featuredProjects: Project[] = [
     link: "https://gce-react-personal-finance.vercel.app/login",
   },
   {
-    image: ReactPersonalFinance,
-    title: "NextJS Audiophile App",
+    image: NextJSAudiophileEcommerce,
+    title: "Next.js Audiophile App",
     description:
       "A Full Stack Audiophile E-Commerce app, a challenge from Frontend Mentor at Guru difficulty.",
     repo: "https://github.com/geraldesteban/nextjs-audiophile-app",
     link: "https://github.com/geraldesteban/nextjs-audiophile-app",
   },
   {
-    image: NextJSTWO,
+    image: ReactTWO,
     title: "React The Wild Oasis App",
     description:
       "A Full Stack Wild Oasis app (Admin POV) built with React from a Udemy tutorial.",
     repo: "https://github.com/geraldesteban/nextjs-the-wild-oasis",
-    link: "https://gce-nextjs-the-wild-oasis.vercel.app/",
+    link: "https://gce-react-the-wild-oasis.vercel.app/",
   },
   {
     image: NextJSTWO,
-    title: "NextJS The Wild Oasis App",
+    title: "Next.js The Wild Oasis App",
     description:
       "A Full Stack Wild Oasis app (User POV) built with Next.js from a Udemy tutorial.",
     repo: "https://github.com/geraldesteban/nextjs-the-wild-oasis",
     link: "https://gce-nextjs-the-wild-oasis.vercel.app/",
   },
   {
-    image: ReactWeddingInvitation,
-    title: "React Wedding Invitation",
+    image: NextJSWeddingInvitation,
+    title: "Next.js Wedding Invitation App",
     description:
-      "A Full Stack Wild Oasis app (User POV) built with Next.js from a Udemy tutorial.",
+      "A Wedding Invitation app built with Next.js, Tailwind CSS, and Supabase.",
     repo: "https://github.com/geraldesteban/wedding-invitation-one",
     link: "https://aris-jane-wedding.vercel.app/",
   },
   {
     image: RestCountriesAPI,
-    title: "Rest Countries API",
+    title: "REST Countries API App",
     description:
-      "I created this simple game along when learning JavaScript DOM. I only use pure HTML, CSS, and JavaScript.",
+      "A REST Countries API app built with JavaScript, Tailwind CSS, and REST Countries API.",
     repo: "https://github.com/geraldesteban/project-rest-countries-api",
     link: "https://rest-countries-api-gce.vercel.app/",
   },
--- app/data/otherProjects.ts
+++ app/data/otherProjects.ts
@@ -1,96 +1,130 @@
-import { OtherProjects } from "../types/otherProjects";
+import { OtherProjectsTypes } from "../types/otherProjects";
 
-export const otherProjects: OtherProjects[] = [
+export const otherProjects: OtherProjectsTypes[] = [
+  {
+    title: "Chat App Illustration",
+    repo: "https://github.com/geraldesteban/frontend-mentor-challenges/tree/main/intermediate-chat-app-illustration",
+    link: "https://frontend-mentor-challenges-fawn-pi.vercel.app/",
+  },
+  {
+    title: "Easy Bank Landing Page",
+    repo: "https://github.com/geraldesteban/frontend-mentor-challenges/tree/main/intermediate-easybank-landing-page",
+    link: "  https://intermediate-easybank-landing-page.vercel.app/",
+  },
+  {
+    title: "Museo ng Kanlahi",
+    repo: "https://github.com/geraldesteban/museo-ng-kanlahi",
+    link: "https://museo-ng-kanlahi.netlify.app/",
+  },
+
+  {
+    title: "Multimodal Text Conversion",
+    repo: "https://github.com/geraldesteban/multimodal-text-conversion",
+    link: "https://multi-interaction-text-conversion.netlify.app/",
+  },
+  {
+    title: "Web Lab Case Study Static",
+    repo: "https://github.com/geraldesteban/web-lab-case-study-static",
+    link: "https://frontend-mentor-gce.netlify.app/",
+  },
+  {
+    title: "Tip Calculator App",
+    repo: "https://github.com/geraldesteban/tip-calculator-app",
+    link: "https://tip-calculator-app-gce.vercel.app/",
+  },
+  {
+    title: "Age Calculator App",
+    repo: "https://github.com/geraldesteban/age-calculator-app",
+    link: "https://age-calculator-app-gce.vercel.app/",
+  },
   {
     title: "Pig Game",
-    description:
-      "I created this simple game along when learning JavaScript DOM. I only use pure HTML, CSS, and JavaScript.",
     repo: "https://github.com/geraldesteban/pig-game",
     link: "https://pig-game-gce.netlify.app/",
   },
   {
     title: "Guess My Number",
-    description:
-      "I created this simple game along when learning JavaScript DOM. I only use pure HTML, CSS, and JavaScript.",
     repo: "https://github.com/geraldesteban/guess-my-number",
     link: "https://guess-my-number-gce.netlify.app/",
   },
   {
-    title: "Web Lab Case Study Static",
-    description:
-      "I created this simple game along when learning JavaScript DOM. I only use pure HTML, CSS, and JavaScript.",
-    repo: "https://github.com/geraldesteban/web-lab-case-study-static",
-    link: "https://frontend-mentor-gce.netlify.app/",
-  },
-  {
-    title: "Multimodal Text Conversion",
-    description:
-      "Text conversion using tesseract.js and web speech recognition.",
-    repo: "https://github.com/geraldesteban/multimodal-text-conversion",
-    link: "https://multi-interaction-text-conversion.netlify.app/",
-  },
-  {
-    title: "Age Calculator App",
-    description:
-      "I created this simple game along when learning JavaScript DOM. I only use pure HTML, CSS, and JavaScript.",
-    repo: "https://github.com/geraldesteban/age-calculator-app",
-    link: "https://age-calculator-app-gce.vercel.app/",
-  },
-  {
-    title: "Tip Calculator App",
-    description:
-      "I created this simple game along when learning JavaScript DOM. I only use pure HTML, CSS, and JavaScript.",
-    repo: "https://github.com/geraldesteban/tip-calculator-app",
-    link: "https://tip-calculator-app-gce.vercel.app/",
-  },
-  {
-    title: "Museo ng Kanlahi",
-    description:
-      "I created this simple game along when learning JavaScript DOM. I only use pure HTML, CSS, and JavaScript.",
-    repo: "https://github.com/geraldesteban/museo-ng-kanlahi",
-    link: "https://museo-ng-kanlahi.netlify.app/",
-  },
-  {
     title: "React Social Link Profile",
-    description:
-      "I created this simple game along when learning JavaScript DOM. I only use pure HTML, CSS, and JavaScript.",
     repo: "https://github.com/geraldesteban/react-social-link-profile",
     link: "https://react-social-link-profile.vercel.app/",
   },
-
   {
     title: "React Recipe Page",
-    description:
-      "I created this simple game along when learning JavaScript DOM. I only use pure HTML, CSS, and JavaScript.",
     repo: "https://github.com/geraldesteban/react-recipe-page",
     link: "https://react-recipe-page.vercel.app/",
   },
   {
     title: "React FAQ Accordion",
-    description:
-      "I created this simple game along when learning JavaScript DOM. I only use pure HTML, CSS, and JavaScript.",
     repo: "https://github.com/geraldesteban/react-interactive-rating-component",
     link: "https://react-faq-accordion-gce.vercel.app/",
   },
   {
     title: "React Interactive Rating Component",
-    description:
-      "I created this simple game along when learning JavaScript DOM. I only use pure HTML, CSS, and JavaScript.",
     repo: "https://github.com/geraldesteban/react-interactive-rating-component",
     link: "https://react-interactive-rating-component-gce.vercel.app/",
   },
   {
     title: "React Ping Single Column Page",
-    description:
-      "I created this simple game along when learning JavaScript DOM. I only use pure HTML, CSS, and JavaScript.",
     repo: "https://github.com/geraldesteban/react-ping-single-column-coming-soon-page",
     link: "https://react-ping-single-column-coming-soon-page.vercel.app/",
   },
   {
     title: "React Base Apparel Page",
-    description:
-      "I created this simple game along when learning JavaScript DOM. I only use pure HTML, CSS, and JavaScript.",
     repo: "https://github.com/geraldesteban/react-base-apparel-coming-soon-page",
     link: "https://react-base-apparel-component-page.vercel.app/",
+  },
+  {
+    title: "Clipboard Landing Page",
+    repo: "https://github.com/geraldesteban/frontend-mentor-challenges/tree/main/junior-clipboard-landing-page",
+    link: "https://junior-clipboard-landing-page.vercel.app/",
+  },
+  {
+    title: "Fylo Data Storage Component",
+    repo: "https://github.com/geraldesteban/frontend-mentor-challenges/tree/main/junior-fylo-data-storage-component",
+    link: "https://junior-fylo-data-storage-component.vercel.app/",
+  },
+  {
+    title: "Fylo Landing Page with Two Column Layout",
+    repo: "https://github.com/geraldesteban/frontend-mentor-challenges/tree/main/junior-fylo-landing-page-with-two-column-layout",
+    link: "https://junior-fylo-landing-page-with-two-c.vercel.app/",
+  },
+  {
+    title: "Huddlde Landing Page with Alternating Feature Blocks",
+    repo: "https://github.com/geraldesteban/frontend-mentor-challenges/tree/main/junior-huddle-landing-page-with-alternating-feature-blocks",
+    link: "https://junior-huddle-landing-page-with-alt.vercel.app/",
+  },
+  {
+    title: "Huddlde Landing Page with Curved Sections",
+    repo: "https://github.com/geraldesteban/frontend-mentor-challenges/tree/main/junior-huddle-landing-page-with-curved-sections",
+    link: "https://junior-huddle-landing-page-with-cur.vercel.app/",
+  },
+  {
+    title: "Testimonials Grid Section",
+    repo: "https://github.com/geraldesteban/frontend-mentor-challenges/tree/main/junior-testimonials-grid-section",
+    link: "https://junior-testimonials-grid-section.vercel.app/",
+  },
+  {
+    title: "Three Column Preview Card Component",
+    repo: "https://github.com/geraldesteban/frontend-mentor-challenges/tree/main/newbie-3-column-preview-card-component",
+    link: "https://newbie-3-column-preview-card-compon.vercel.app/",
+  },
+  {
+    title: "Article Preview Component",
+    repo: "https://github.com/geraldesteban/frontend-mentor-challenges/tree/main/newbie-article-preview-component",
+    link: "https://newbie-article-preview-component-amber.vercel.app/",
+  },
+  {
+    title: "Four Card Feature Section",
+    repo: "https://github.com/geraldesteban/frontend-mentor-challenges/tree/main/newbie-four-card-feature-section",
+    link: "https://newbie-3-column-preview-card-compon.vercel.app/",
+  },
+  {
+    title: "Three Column Preview Card Component",
+    repo: "https://github.com/geraldesteban/frontend-mentor-challenges/tree/main/newbie-3-column-preview-card-component",
+    link: "https://newbie-3-column-preview-card-compon.vercel.app/",
   },
 ];
